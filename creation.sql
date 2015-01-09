ef0d8dffc91f08ccbf15f66327c43c2fe873f5cf
Ivan Gromakovsky <gie1994@gmail.com>
2015-01-09 21:52:13 +0300

    Add first version of creation script

---
+++ creation.sql
@@ -0,0 +1,118 @@
+-- Entities
+
+CREATE TABLE IF NOT EXISTS Nodes
+(
+    node_id         bigint              PRIMARY KEY,
+    version         int                 NOT NULL DEFAULT 1,
+    latitude        double precision    NOT NULL,
+    longitude       double precision    NOT NULL,
+    timestamp       timestamp           NOT NULL,
+
+    user_id         int                 NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Ways
+(
+    way_id          bigint              PRIMARY KEY,
+    version         int                 NOT NULL DEFAULT 1,
+
+    user_id         int                 NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Relations
+(
+    relation_id     bigint              PRIMARY KEY,
+    version         int                 NOT NULL DEFAULT 1,
+    timestamp       timestamp           NOT NULL,
+
+    user_id         int                 NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Tags
+(
+    tag_key         text                PRIMARY KEY,
+    introduced_date date                NOT NULL,
+    description     text                ,
+
+    introduced_by   int                 NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Users
+(
+    user_id         int                 PRIMARY KEY,
+    user_name       text                NOT NULL,
+    email           text                NOT NULL,
+    password_hash   text                NOT NULL,
+    status          text                NOT NULL,
+    information     text
+);
+
+-- Relationships
+
+ALTER TABLE Nodes ADD FOREIGN KEY(user_id) REFERENCES Users(user_id);
+ALTER TABLE Ways ADD FOREIGN KEY(user_id) REFERENCES Users(user_id);
+ALTER TABLE Relations  ADD FOREIGN KEY(user_id) REFERENCES Users(user_id);
+ALTER TABLE Tags  ADD FOREIGN KEY(introduced_by) REFERENCES Users(user_id);
+
+-- Associations
+
+CREATE TABLE IF NOT EXISTS NodesInRelations
+(
+    role            text                NOT NULL,
+
+    node_id         bigint              NOT NULL REFERENCES Nodes(node_id),
+    relation_id     bigint              NOT NULL REFERENCES Relations(relation_id),
+
+    UNIQUE(node_id, relation_id)
+);
+
+CREATE TABLE IF NOT EXISTS NodesInWays
+(
+    node_index      int                 NOT NULL,
+
+    node_id         bigint              NOT NULL REFERENCES Nodes(node_id),
+    way_id          bigint              NOT NULL REFERENCES Ways(way_id),
+
+    UNIQUE(node_id, way_id)
+);
+
+CREATE TABLE IF NOT EXISTS WaysInRelations
+(
+    role            text                NOT NULL,
+
+    way_id          bigint              NOT NULL REFERENCES Ways(way_id),
+    relation_id     bigint              NOT NULL REFERENCES Relations(relation_id),
+
+    UNIQUE(way_id, relation_id)
+);
+
+CREATE TABLE IF NOT EXISTS NodeTags
+(
+    tag_value       text                NOT NULL,
+
+    node_id         bigint              NOT NULL REFERENCES Nodes(node_id),
+    tag_key         text                NOT NULL REFERENCES Tags(tag_key),
+
+    UNIQUE(node_id, tag_key)
+);
+
+CREATE TABLE IF NOT EXISTS WayTags
+(
+    tag_value       text                NOT NULL,
+
+    way_id          bigint              NOT NULL REFERENCES Ways(way_id),
+    tag_key         text                NOT NULL REFERENCES Tags(tag_key),
+
+    UNIQUE(way_id, tag_key)
+);
+
+CREATE TABLE IF NOT EXISTS RelationTags
+(
+    tag_value       text                NOT NULL,
+
+    relation_id     bigint              NOT NULL REFERENCES Relations(relation_id),
+    tag_key         text                NOT NULL REFERENCES Tags(tag_key),
+
+    UNIQUE(relation_id, tag_key)
+);
+
