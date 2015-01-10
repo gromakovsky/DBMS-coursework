6864706803e6b575b50573cc7c7cb3f248a0a06f
Ivan Gromakovsky <gie1994@gmail.com>
2015-01-10 22:05:36 +0300

    Update clear.sql

--- clear.sql
+++ clear.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS Nodes, Ways, Relations, Tags, Users CASCADE;
 DROP TABLE IF EXISTS NodesInRelations, NodesInWays, WaysInRelations, NodeTags, WayTags, RelationTags CASCADE;
 
 DROP FUNCTION IF EXISTS relation_size(bigint);
+DROP FUNCTION IF EXISTS add_user(int, text, text, text);
 DROP FUNCTION IF EXISTS check_relation_content();
 DROP FUNCTION IF EXISTS check_timestamp_and_version();
 
